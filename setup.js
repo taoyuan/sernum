31138825911c783a2c9b7aba399d3621286e33de
Yuan Tao <towyuan@outlook.com>
2017-06-19 14:41:33 +0800

    Refactor

--- setup.js
+++ setup.js
@@ -1,14 +1,21 @@
-
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const sn = require('.');
+
+const cacheFile = path.join(__dirname, 'cache');
 
 const fail = function (err) {
   console.error('Could not read serial number:', err);
 };
 
+if (fs.existsSync(cacheFile)) {
+  fs.unlinkSync(cacheFile);
+}
+
 try {
-  sn();
+  sn({ignoreCache: true});
 } catch (err) {
   if (process.platform !== 'win32' && err && err.toString().match(/Permission denied/i)) {
     [
@@ -25,7 +32,7 @@ try {
       const val = sn.sudo();
 
       try {
-        require('fs').writeFileSync(__dirname + '/cache', val);
+        fs.writeFileSync(cacheFile, val);
         console.info('\x1B[32m' + 'Successfully cached serial number' + '\x1B[39m');
       } catch (err) {
         console.error('Could not write serial number cache file:', err);
